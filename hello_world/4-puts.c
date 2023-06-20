053d42732976967f8cd2a7e69859993142f8f6ff
Dancolm95 <dancolm95@gmail.com>
2023-06-19 21:25:17 -0700

    write a C program that prints exactly

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,14 @@
 #include<stdio.h>
+
+/**
+ * main - Starting program
+ *
+ * Description: Print
+ *
+ * Return: zero
+ */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
+
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0) ; }
