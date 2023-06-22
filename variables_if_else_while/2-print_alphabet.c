eaacd562a0726f1c922b6a13d5cb0c09bad0a31f
Dancolm95 <dancolm95@gmail.com>
2023-06-21 19:24:10 -0700

    Program that writes the alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Alphabet
+ *
+ * return: Always
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
