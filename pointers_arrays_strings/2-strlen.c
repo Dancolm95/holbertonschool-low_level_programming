9bbe404856e1d7fadd3b69e9cbdad9f83b6fded6
Dancolm95 <dancolm95@gmail.com>
2023-06-26 21:56:38 -0700

    Function that returns the lenght of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - length of string
+ * @s: Character
+ *
+ * Return: Zero if succesful
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+		;
+	return (a);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,5 +3,6 @@
 
 void reset_to_98(int *n);
 int _putchar(char c);
+int _strlen(char *s);
 
 #endif /* _main.h_ */
