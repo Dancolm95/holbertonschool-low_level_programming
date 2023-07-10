61f01f8fa7ee25ea7e3fa063c50e6d19d3a5a32c
Dancolm95 <dancolm95@gmail.com>
2023-07-09 20:58:02 -0700

    Write a function that prints strings, followed by a new line.

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,10 @@
 #ifndef _variadic_functions_h_
 #define _variadic_functions_h_
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif /* _variadic_functions.h_ */
